85733e6b55846f1db07123b0dc2afe0d9850de3e
Harrison Royds <harrisonroyds@yahoo.co.uk>
2024-01-31 11:43:43 +0000

    extra task promisification done

--- challenges/3-fetch-all-cats.js
+++ challenges/3-fetch-all-cats.js
@@ -1,14 +1,22 @@
 const fetchCatsByOwner = require("./1-fetch-cats-by-owner");
 const fetchAllOwners = require("./2-fetch-all-owners");
 
-const fetchAllCats = () => {
-  return fetchAllOwners().then((owners) => {
-    return Promise.all(owners.map((owner) => fetchCatsByOwner(owner))).then(
-      (cats) => {
-        return cats.flat().sort();
-      }
-    );
-  });
+// const fetchAllCats = () => {
+//   return fetchAllOwners().then((owners) => {
+//     return Promise.all(owners.map((owner) => fetchCatsByOwner(owner))).then(
+//       (cats) => {
+//         return cats.flat().sort();
+//       }
+//     );
+//   });
+// };
+
+const fetchAllCats = async () => {
+  const owners = await fetchAllOwners();
+  const cats = await Promise.all(
+    owners.map((owner) => fetchCatsByOwner(owner))
+  );
+  return cats.flat().sort();
 };
 
 module.exports = fetchAllCats;
--- extra-challenges/promisification.js
+++ extra-challenges/promisification.js
@@ -1,14 +1,23 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 // do not alter this function
 function readSecretFile(cb) {
-  const filePath = path.join(__dirname, '../challenges/secret-message.txt');
-  fs.readFile(filePath, 'utf8', cb);
+  const filePath = path.join(__dirname, "../challenges/secret-message.txt");
+  fs.readFile(filePath, "utf8", cb);
 }
 
 function promisifiedReadSecretFile() {
   // your code here
+  return new Promise((resolve, reject) => {
+    readSecretFile((err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
 }
 
 module.exports = { readSecretFile, promisifiedReadSecretFile };
